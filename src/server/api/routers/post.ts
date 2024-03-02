bb58af85c442b4b973a8c20e80e66c476c338e13
Kees Jansen <jansengsy@gmail.com>
2024-03-02 17:28:34 +0000

    base feed with emoji tweets done

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,46 @@
+import { auth, clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/server";
+import { TRPCClientError } from "@trpc/client";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+// Creating a filter so that we don't return all the user data to the client
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.firstName,
+    profilePicture: user.imageUrl,
+  };
+};
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(( {ctx} ) => {
-    return ctx.db.post.findMany();
-  })
-})
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+
+      return {
+        post,
+        author,
+      };
+    });
+  }),
+});
