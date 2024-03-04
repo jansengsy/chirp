b4990d8aace76f806f4b5ce2186cde09438f9707
Kees Jansen <jansengsy@gmail.com>
2024-03-04 09:36:43 +0000

    text sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username} `}</span>
           <span className="font-thin">{` • ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
